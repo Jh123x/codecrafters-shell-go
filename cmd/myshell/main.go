2172b1188dfcd1f6b82a34f215d792cdc9a065e0
Wen Junhua <wenjh1998@gmail.com>
2024-05-26 12:14:00 +0800

    feat: add repl loop

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -5,25 +5,29 @@ import (
 	"os"
 
 	"github.com/codecrafters-io/shell-starter-go/cmd/myshell/command"
+	"github.com/codecrafters-io/shell-starter-go/cmd/myshell/consts"
 	"github.com/codecrafters-io/shell-starter-go/cmd/myshell/parser"
 )
 
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Print("$ ")
+	for {
+		fmt.Print("$ ")
+		cmd, args, err := parser.ParseFromReader(os.Stdin)
+		if err != nil {
+			fmt.Println("Error reading command")
+		}
 
-	// Wait and parse user input
-	cmd, args, err := parser.ParseFromReader(os.Stdin)
-	if err != nil {
-		fmt.Println("Error reading command")
+		if cmd == consts.EXIT {
+			return
+		}
+
+		// Handle Command
+		output, err := command.HandleCommand(cmd, args)
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+
+		fmt.Println(output)
 	}
-
-	// Handle Command
-	output, err := command.HandleCommand(cmd, args)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	fmt.Println(output)
 }
