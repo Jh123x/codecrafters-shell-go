8b62dbf67cec0bd86c3dc1a69eb1012bfc5c3bef
Junhua <wenjh1998@gmail.com>
2024-05-29 22:49:12 +0800

    feat: make cd command return to home by default

--- cmd/myshell/command/chdir.go
+++ cmd/myshell/command/chdir.go
@@ -8,18 +8,24 @@ import (
 )
 
 func ChangeDir(args []string) (string, error) {
-	if len(args) != 1 {
+	if len(args) > 1 {
 		return "", consts.ErrChdirUsage
 	}
 
-	dir := args[0]
-	if dir == "~" {
+	dir := ""
+	if len(args) == 0 || args[0] == "~" {
 		dir = os.Getenv(consts.ENV_HOME)
+	} else {
+		dir = args[0]
 	}
 
+	return "", changeDir(dir)
+}
+
+func changeDir(dir string) error {
 	if err := os.Chdir(dir); err != nil {
-		return "", fmt.Errorf("cd: %s: No such file or directory", dir)
+		return fmt.Errorf("cd: %s: No such file or directory", dir)
 	}
 
-	return "", nil
+	return nil
 }
