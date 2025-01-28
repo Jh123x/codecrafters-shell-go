28273874b29b455bd59ebdfa509c7d625f43a14f
Wen Junhua <wenjh1998@gmail.com>
2025-01-29 00:51:18 +0800

    tests: add test for write to file/append to file

---
+++ cmd/myshell/command/file_write_test.go
@@ -0,0 +1,88 @@
+package command
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWriteToFil(t *testing.T) {
+	tmpDir := filepath.Join(os.TempDir(), "test_create_dir")
+	assert.Nil(t, os.Mkdir(tmpDir, 0644))
+	defer func() { assert.Nil(t, os.RemoveAll(tmpDir)) }()
+
+	tests := map[string]struct {
+		setUp    func(*testing.T)
+		filePath string
+		contents string
+	}{
+		"file which exists": {
+			setUp: func(t *testing.T) {
+				path := filepath.Join(tmpDir, "exists.txt")
+				fmt.Println(path)
+				assert.Nil(t, os.WriteFile(path, []byte("some other data"), 0644))
+			},
+			filePath: filepath.Join(tmpDir, "exists.txt"),
+			contents: "some content",
+		},
+		"file which does not exists": {
+			filePath: filepath.Join(tmpDir, "other.txt"),
+			contents: "some content",
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			if tc.setUp != nil {
+				tc.setUp(t)
+			}
+
+			assert.Nil(t, WriteToFile(tc.filePath, tc.contents))
+			res, err := os.ReadFile(tc.filePath)
+			assert.Nil(t, err)
+			assert.Equal(t, tc.contents, string(res))
+		})
+	}
+}
+
+func TestAppendToFile(t *testing.T) {
+	tmpDir := filepath.Join(os.TempDir(), "test_append_dir")
+	assert.Nil(t, os.Mkdir(tmpDir, 0644))
+	defer func() { assert.Nil(t, os.RemoveAll(tmpDir)) }()
+
+	tests := map[string]struct {
+		setUp    func(*testing.T)
+		filePath string
+		contents string
+	}{
+		"file which exists": {
+			setUp: func(t *testing.T) {
+				path := filepath.Join(tmpDir, "exists.txt")
+				fmt.Println(path)
+				assert.Nil(t, os.WriteFile(path, []byte("some other data\n"), 0644))
+			},
+			filePath: filepath.Join(tmpDir, "exists.txt"),
+			contents: "some other data\nsome content",
+		},
+		"file which does not exists": {
+			filePath: filepath.Join(tmpDir, "other.txt"),
+			contents: "some content",
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			if tc.setUp != nil {
+				tc.setUp(t)
+			}
+
+			assert.Nil(t, AppendToFile(tc.filePath, tc.contents))
+			res, err := os.ReadFile(tc.filePath)
+			assert.Nil(t, err)
+			assert.Equal(t, tc.contents, string(res))
+		})
+	}
+}
